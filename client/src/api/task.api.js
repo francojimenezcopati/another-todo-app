aab1adb2b94ee536d41cbec840b95be6c9b84393
Francojimenezcopati <fjcopati@gmail.com>
2024-02-25 13:15:28 -0300

    set deploy url

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,55 +1,54 @@
-
-const BASE_URL = 'http://localhost:8000/api/tasks/'
+const BASE_URL = "another-todo-app.up.railway.app/api/tasks";
 
 export const getTasks = async () => {
-    const response = await fetch(BASE_URL)
-    const data = await response.json()
-    return data
-}
+    const response = await fetch(BASE_URL);
+    const data = await response.json();
+    return data;
+};
 
 export const createTask = (task) => {
     fetch(BASE_URL, {
-        method: 'POST', // Especifica que esta es una solicitud POST
+        method: "POST", // Especifica que esta es una solicitud POST
         headers: {
-            'Content-Type': 'application/json' // Indica que el cuerpo de la solicitud es JSON
+            "Content-Type": "application/json", // Indica que el cuerpo de la solicitud es JSON
         },
-        body: JSON.stringify(task) // Convierte el objeto de tarea a formato JSON JSON.stringify(task)
+        body: JSON.stringify(task), // Convierte el objeto de tarea a formato JSON JSON.stringify(task)
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error al realizar la solicitud:', error));
-    
-    return 
-}
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error al realizar la solicitud:", error));
+
+    return;
+};
 
 export const deleteTask = (id) => {
     fetch(BASE_URL + `${id}/`, {
-        method: 'DELETE', 
+        method: "DELETE",
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error al realizar la solicitud:', error));
-    
-    return 
-}
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error al realizar la solicitud:", error));
+
+    return;
+};
 
 export const updateTask = (id, task) => {
     fetch(BASE_URL + `${id}/`, {
-        method: 'PUT', 
+        method: "PUT",
         headers: {
-            'Content-Type': 'application/json' 
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(task) 
+        body: JSON.stringify(task),
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error al realizar la solicitud:', error));
-    
-    return 
-}
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error al realizar la solicitud:", error));
+
+    return;
+};
 
 export const getTaskDetails = async (id) => {
-    const response = await fetch(BASE_URL + `${id}/`)
-    const data = await response.json()
-    return data
-}
+    const response = await fetch(BASE_URL + `${id}/`);
+    const data = await response.json();
+    return data;
+};
